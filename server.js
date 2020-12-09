ddeb6f4030105fd36210feb34c7775cf8612277b
Pedro A. González <pgonzalez@post.com>
2020-12-08 21:10:52 -0500

    Agregar documentación en html

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+const path = require('path');
 
 // MIDDLEWARE
 app.use(cors());
@@ -10,8 +11,13 @@ app.use(express.json());
 
 // ENDPOINTS
 app.get('/', (req, res) => {
-  res.send('API Firebase B47');
+  res.send('API Firebase B47 <a href="/docs">/docs</a> 📄');
 })
+
+// DOCUMENTACIÓN
+app.get('/docs', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // RUTAS API
 app.use('/api/v1', require('./router'));
